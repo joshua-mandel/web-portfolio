8a23cf6642fe518bf505f47480c101f866e66451
joshua-mandel <89474480+joshua-mandel@users.noreply.github.com>
2023-03-07 15:27:29 -0600

    update

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -1,7 +1,24 @@
 import {defineConfig} from 'sanity/lib/exports'
 import {deskTool} from 'sanity/desk'
-import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
+import type {ComponentType} from 'react'
+import {Plugin as Plugin_2} from 'sanity'
+
+declare interface VisionConfig {
+  defaultApiVersion: string
+  defaultDataset?: string
+}
+
+export declare const visionTool: Plugin_2<void | VisionToolConfig>
+
+export declare interface VisionToolConfig extends Partial<VisionConfig> {
+  name?: string
+  title?: string
+  icon?: ComponentType
+}
+
+export {}
+
 
 export default defineConfig({
   name: 'default',
